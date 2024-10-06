7300694644858d2ee3b6a9a9112711c04cf002c7
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-10-06 21:14:41 +0000

    promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('THe fake API is not working currently'));
+    }
+  });
+}
