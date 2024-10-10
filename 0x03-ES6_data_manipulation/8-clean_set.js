2185b71cebd4d46425d2b9b23555fab2989488e8
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-10-10 23:53:27 +0000

    set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,8 +2,11 @@ export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') {
     return '';
   }
-  return [...set]
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
-    .join('-');
+  const result = [];
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  }
+  return result.join('-');
 }
