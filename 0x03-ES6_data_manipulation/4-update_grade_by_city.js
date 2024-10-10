f697743f2091c0ddbb7e302e7eaf528037ab5840
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-10-10 22:07:16 +0000

    combine map and filter

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-      const studentGrade = newGrades.find((gradeObj) => gradeObj.student === student.id);
+      const studentGrade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
       return {
         ...student,
         grade: studentGrade ? studentGrade.grade : 'N/A',
