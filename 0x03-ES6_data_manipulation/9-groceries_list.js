868ec2f607cdc7cdf4b67e5f0f4f0dc39de6527d
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-10-11 00:08:21 +0000

    Grocies list

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const gro = new Map();
+  gro.set('Apples', 10);
+  gro.set('Tomatoes', 10);
+  gro.set('Pasta', 1);
+  gro.set('Rice', 1);
+  gro.set('Banana', 5);
+
+  return gro;
+}
