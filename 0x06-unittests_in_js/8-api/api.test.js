74d6f71eb1d8426a6cc686f85ce6082e7f324757
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-29 22:55:15 +0100

    Payment

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,38 +1,24 @@
-const chai = require('chai');
 const request = require('request');
-const expect = chai.expect;
+const { expect } = require('chai');
 
-describe('Index page', () => {
-  const baseUrl = 'http://localhost:7865';
-
-  before((done) => {
-    // Start the server
-    const app = require('./api');
-    this.server = app.listen(7865, done);
-  });
-
-  after((done) => {
-    // Close the server
-    this.server.close(done);
-  });
-
-  it('Correct status code?', (done) => {
-    request.get(`${baseUrl}/`, (error, response, body) => {
+describe('Index page', function () {
+  it('should have the correct status code', function (done) {
+    request('http://localhost:7865', function (error, response) {
       expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
-  it('Correct result?', (done) => {
-    request.get(`${baseUrl}/`, (error, response, body) => {
+  it('should have the correct result', function (done) {
+    request('http://localhost:7865', function (error, response, body) {
       expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
 
-  it('Other?', (done) => {
-    request.get(`${baseUrl}/`, (error, response, body) => {
-      expect(response.headers['content-type']).to.contain('text/html');
+  it('other?', function (done) {
+    request('http://localhost:7865', function (error, response, body) {
+      expect(body).to.not.equal('Something else');
       done();
     });
   });
