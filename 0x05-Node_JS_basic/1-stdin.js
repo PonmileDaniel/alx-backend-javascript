b282ca469e995cb7974844bd64859b9b116b9ef8
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-29 10:14:34 +0100

    Using standard input

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-    const input = data.toString().trim();
-    process.stdin.write(`Your name is: ${input}\n`)
-    process.stdin.end();
+    process.stdin.write(`Your name is: ${data}`)
 });
 
 process.stdin.on('end', () => {
