f53b516a6ac4da442bc7943d49f76cbb6b0b4450
root <root@DESKTOP-ODC38F9.localdomain>
2024-11-30 10:23:36 +0100

    Calculate number

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,9 +3,9 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
     it('should return the sum of two numbers rounded off', function () {
-        assert.strictEqual(calculateNumber(1, 3), 4);
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+        assert.strictEqual(calculateNumber(1, 3), 4);        // 1 + 3
+        assert.strictEqual(calculateNumber(1, 3.7), 5);      // 1 + 4
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);    // 1 + 4
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);    // 2 + 4
     });
 });
